7566e37db2f5a5148015494df5f8ce17d6cc9167
dell <patilayush8788@gmail.com>
2023-11-10 09:57:29 +0530

    new

--- lib/folders/home_page.dart
+++ lib/folders/home_page.dart
@@ -3,30 +3,36 @@ import 'dart:ffi';
 import 'package:app2/widgets/drawer.dart';
 import 'package:flutter/material.dart';
 import 'package:app2/models/lists.dart';
-
+import 'package:velocity_x/velocity_x.dart';
 import 'package:app2/widgets/items.dart';
 class HomePage extends StatelessWidget{
-  final int days=1;
-  final String name="India";
+
   @override
   Widget build(BuildContext context){
 
-    final dummylist=List.generate(10, (index) => list.products[0]);
+
 
 
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Elderly Companionship App"),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: ListView.builder(itemCount: dummylist.length,
-        itemBuilder: (context,index){
+      body:SafeArea(
+        child: Container(
+          padding: Vx.m32,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              CatalogHeader(),
+              if(list.products!=null && list.products.isNotEmpty)
+                Itemlist()
+              else
+                Center(child: CircularProgressIndicator()),
 
-          return  ItemWidget(
-          item:dummylist[index], );
-        }),
+
+
+
+            ],
+          ),
+        ),
       ),
       drawer: Mydrawer(),
 
@@ -35,4 +41,55 @@ class HomePage extends StatelessWidget{
   }
 
 
+}
+
+class CatalogHeader extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return Column(
+      children: [
+
+        "Catalog App".text.xl5.bold.color(Colors.indigo).make(),
+        "recent products".text.xl2.color(Colors.indigoAccent).make(),
+      ],
+
+
+    );
+  }
+}
+
+
+class Itemlist extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: list.products.length,
+      itemBuilder: (context, index) {
+        final catalog = list.products[index];
+        return CatalogItem(catalog: catalog);
+      },
+
+
+    );
+  }
+}
+
+
+class CatalogItem extends StatelessWidget {
+@override
+final Item catalog;
+
+  const CatalogItem({super.key, required this.catalog});
+Widget build(BuildContext context) {
+  return VxBox(
+    child: Row(
+      children: [
+        Image.network(catalog.image)
+      ],
+    ),
+
+  ).white.square(120).make();
+}
 }
